cc3dd6a8b14b5cc8e84085a72c82649e46ba5cb8
arnolanglade <alanglade@palo-it.com>
2022-11-23 12:24:07 +0100

    fix(react): add missing )

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -6,25 +6,25 @@ import 'react-native';
 import React from 'react';
 import renderer from 'react-test-renderer';
 import { renderHook } from '@testing-library/react-native';
-import App, {TodoStatus, useTodoList} from './App';
+import App, { TodoStatus, useTodoList } from './App';
 
 // Note: test renderer must be required after react-native.
 
 const aTodoList = () => ({
-    id: 1,
-    label: 'label',
-    description: 'description',
-    status: TodoStatus.TODO,
-    assignee: {
-        name: 'Dupond',
-        firstname: 'Henri',
-        // eslint-disable-next-line global-require
-        image: require('../asset/images/portrait-homme-blanc-isole_53876-40306.webp'),
-    },
-})
+  id: 1,
+  label: 'label',
+  description: 'description',
+  status: TodoStatus.TODO,
+  assignee: {
+    name: 'Dupond',
+    firstname: 'Henri',
+    // eslint-disable-next-line global-require
+    image: require('../asset/images/portrait-homme-blanc-isole_53876-40306.webp'),
+  },
+});
 
 it('renders correctly', () => {
-    const todolist = renderHook(() => useTodoList());
+  const todolist = renderHook(() => useTodoList());
 
-    expect(todolist.result.current.todolist).toEqual(aTodoList());
-}
+  expect(todolist.result.current.todolist).toEqual(aTodoList());
+});
