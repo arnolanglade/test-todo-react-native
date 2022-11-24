e9a9d9a6cc113d4be26e9b2211ac3d93dbc5a6a5
Thibaut <thibaut.mejane@gmail.com>
2022-11-24 12:26:53 +0100

    fix: queryTodoItem params

--- src/app/ContainerContext.tsx
+++ src/app/ContainerContext.tsx
@@ -2,6 +2,6 @@ import { createContext } from 'react';
 import { TodoItem, TodoList } from '../domain/TodoList';
 import { queryTodoList, queryTodoItem } from '../service/QueryTodoList';
 
-export type ServiceContainer = { queryTodoList: () => TodoList, queryTodoItem: () => TodoItem };
-export const ContainerContext = createContext<ServiceContainer>({ queryTodoList: () => [], queryTodoItem: () => {} });
+export type ServiceContainer = { queryTodoList: () => TodoList, queryTodoItem: (id:number) => TodoItem };
+export const ContainerContext = createContext<ServiceContainer>({ queryTodoList: () => [], queryTodoItem: (id:number) => {} });
 export const container = { queryTodoList, queryTodoItem };
