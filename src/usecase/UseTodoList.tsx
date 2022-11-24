551d6869b10f8fc96dac1ed6fec85d471b5d77de
Steve Alves <salves@palo-it.com>
2022-11-24 15:13:02 +0100

    refactor: clean itemtodolist test

--- src/usecase/UseTodoList.tsx
+++ src/usecase/UseTodoList.tsx
@@ -3,17 +3,14 @@ import { ContainerContext } from '../app/ContainerContext';
 import { TodoList } from '../domain/TodoList';
 
 export const useTodoList = () => {
-  const { queryTodoList, queryTodoItem } = useContext(ContainerContext);
+  const { queryTodoList } = useContext(ContainerContext);
   const [todoList, setTodoList] = useState<TodoList>([]);
 
   useEffect(() => {
     setTodoList(queryTodoList());
   }, []);
 
-  const getTodoItem = (id:number) => queryTodoItem(id);
-
   return {
     todoList,
-    getTodoItem,
   };
 };
