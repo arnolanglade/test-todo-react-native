99e03ac96f5a59e66d94b1a4bc2b82fc0350e5e8
Thibaut <thibaut.mejane@gmail.com>
2022-11-24 17:35:08 +0100

    feat: navigation between screens

--- src/view/screen/TodoListScreen.tsx
+++ src/view/screen/TodoListScreen.tsx
@@ -1,20 +1,28 @@
 import React, { ReactElement } from 'react';
-import { FlatList, Text, View } from 'react-native';
+import {
+  FlatList, Text, TouchableOpacity, View,
+} from 'react-native';
 import FastImage from 'react-native-fast-image';
 import { TodoItem } from '../../domain/TodoList';
 import { useTodoList } from '../../usecase/UseTodoList';
 
-export default function TodoListScreen() {
+export default function TodoListScreen({ navigation }) {
   const { todoList } = useTodoList();
 
+  const navigateToDetails = () => {
+    navigation.navigate('Item');
+  };
+
   const renderTodoItem = ({ item }: { item: TodoItem }): ReactElement => (
-    <View style={{
-      paddingTop: 15,
-      paddingBottom: 15,
-      paddingLeft: 10,
-      paddingRight: 10,
-      flexDirection: 'row',
-    }}
+    <TouchableOpacity
+      onPress={navigateToDetails}
+      style={{
+        paddingTop: 15,
+        paddingBottom: 15,
+        paddingLeft: 10,
+        paddingRight: 10,
+        flexDirection: 'row',
+      }}
     >
       <View style={{ flex: 1, backgroundColor: 'salmon' }}>
         <FastImage
@@ -30,7 +38,7 @@ export default function TodoListScreen() {
         <Text>{item.assignee.name}</Text>
         <Text>{item.assignee.firstname}</Text>
       </View>
-    </View>
+    </TouchableOpacity>
   );
 
   return <FlatList data={todoList} renderItem={renderTodoItem} />;
