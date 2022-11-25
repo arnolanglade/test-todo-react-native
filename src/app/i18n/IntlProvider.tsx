75a6d152a69b6ae3e45ad575cb8345cd13023d73
arnolanglade <alanglade@palo-it.com>
2022-11-25 16:56:51 +0100

    feat: add overriddenMessages to intl provider

--- src/app/i18n/IntlProvider.tsx
+++ src/app/i18n/IntlProvider.tsx
@@ -1,14 +1,22 @@
 import React, { ReactElement } from 'react';
 import { IntlProvider as BaseIntlProvider } from 'react-intl';
 
-const messages = {
+type Translations = Record<string, string>;
+
+const messages: Translations = {
   text: 'This is a test',
 };
 
-export default function IntlProvider({ children }:{ children: ReactElement }) {
+export default function IntlProvider(
+  { children, overriddenMessages }:{ children: ReactElement, overriddenMessages?: Translations },
+) {
   return (
-    <BaseIntlProvider messages={messages} locale="en" defaultLocale="en">
+    <BaseIntlProvider messages={{ ...messages, ...overriddenMessages }} locale="en" defaultLocale="en">
       {children}
     </BaseIntlProvider>
   );
 }
+
+IntlProvider.defaultProps = {
+  overriddenMessages: undefined,
+};
