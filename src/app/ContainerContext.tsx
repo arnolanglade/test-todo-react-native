a631c6a218b586a579eb77b46fb17dbde7519059
arnolanglade <alanglade@palo-it.com>
2022-11-25 12:26:20 +0100

    fix: fix container context default value

--- src/app/ContainerContext.tsx
+++ src/app/ContainerContext.tsx
@@ -2,7 +2,10 @@ import { createContext } from 'react';
 import { TodoItem, TodoList } from '../domain/TodoList';
 import { queryTodoList, queryTodoItem } from '../service/QueryTodoList';
 
-export type ServiceContainer = { queryTodoList: () => TodoList, queryTodoItem: (id:number) => TodoItem };
+export type ServiceContainer = undefined | {
+  queryTodoList: () => TodoList,
+  queryTodoItem: (id:number) => TodoItem
+};
 
-export const ContainerContext = createContext<ServiceContainer>({ queryTodoList: () => [], queryTodoItem: (id:number) => {} });
+export const ContainerContext = createContext<ServiceContainer>(undefined);
 export const container = { queryTodoList, queryTodoItem };
