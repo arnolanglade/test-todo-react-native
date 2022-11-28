da68321c5c364c156a9c80aea06745960efb39c8
Alexandre <abirba@palo-it.com>
2022-11-28 10:30:12 +0100

    test: rename mytranslationKey to myTranslationKey

--- src/app/i18n/IntlProvider.test.tsx
+++ src/app/i18n/IntlProvider.test.tsx
@@ -13,27 +13,27 @@ describe('Intl', () => {
     it('renders the correct translation', () => {
       const intl = renderHook(
         () => useIntl(),
-        { wrapper: createWrapper(container, { mytranslationKey: 'translation message' }) },
+        { wrapper: createWrapper(container, { myTranslationKey: 'translation message' }) },
       );
 
-      expect(intl.result.current.translation('mytranslationKey')).toEqual('translation message');
+      expect(intl.result.current.translation('myTranslationKey')).toEqual('translation message');
     });
 
     it('renders the correct value', () => {
       const intl = renderHook(
         () => useIntl(),
-        { wrapper: createWrapper(container, { mytranslationKey: 'translation {message}' }) },
+        { wrapper: createWrapper(container, { myTranslationKey: 'translation {message}' }) },
       );
 
-      expect(intl.result.current.translation('mytranslationKey', { message: 'here is my value' })).toEqual('translation here is my value');
+      expect(intl.result.current.translation('myTranslationKey', { message: 'here is my value' })).toEqual('translation here is my value');
     });
   });
 
   describe('Translation component', () => {
     it('renders the Translation tag', () => {
       render(
-        <Text><Translation id="mytranslationKey" /></Text>,
-        { wrapper: createWrapper(container, { mytranslationKey: 'translation message' }) },
+        <Text><Translation id="myTranslationKey" /></Text>,
+        { wrapper: createWrapper(container, { myTranslationKey: 'translation message' }) },
       );
 
       expect(screen.getByText('translation message')).toBeTruthy();
@@ -41,8 +41,8 @@ describe('Intl', () => {
 
     it('renders the Translation tag with value', () => {
       render(
-        <Text><Translation id="mytranslationKey" values={{ message: 'here is my value' }} /></Text>,
-        { wrapper: createWrapper(container, { mytranslationKey: 'translation {message}' }) },
+        <Text><Translation id="myTranslationKey" values={{ message: 'here is my value' }} /></Text>,
+        { wrapper: createWrapper(container, { myTranslationKey: 'translation {message}' }) },
       );
 
       expect(screen.getByText('translation here is my value')).toBeTruthy();
@@ -52,9 +52,9 @@ describe('Intl', () => {
   it('renders the todo item details', () => {
     const intl = renderHook(
       () => useIntl(),
-      { wrapper: createWrapper(container, { mytranslationKey: 'translation {message}' }) },
+      { wrapper: createWrapper(container, { myTranslationKey: 'translation {message}' }) },
     );
 
-    expect(intl.result.current.translation('mytranslationKey', { message: 'here is my value' })).toEqual('translation here is my value');
+    expect(intl.result.current.translation('myTranslationKey', { message: 'here is my value' })).toEqual('translation here is my value');
   });
 });
