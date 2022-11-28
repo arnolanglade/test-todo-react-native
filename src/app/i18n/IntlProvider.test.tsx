ae3cddb242a46834abf4531d3ff490a942ff92b8
Steve Alves <salves@palo-it.com>
2022-11-28 10:11:29 +0100

    test(intl): add 2 tests: one about the value and the other about the Translation tag

--- src/app/i18n/IntlProvider.test.tsx
+++ src/app/i18n/IntlProvider.test.tsx
@@ -1,17 +1,38 @@
+import React from 'react';
 import {
-  renderHook,
+  render,
+  renderHook, screen,
 } from '@testing-library/react-native';
+import { Text } from 'react-native';
 import { container } from '../ContainerContext';
 import { createWrapper } from '../testing/WrapperUtils';
-import { useIntl } from './IntlProvider';
+import { Translation, useIntl } from './IntlProvider';
 
 describe('Intl Provider', () => {
-  it('renders the todo item details', () => {
+  it('renders the correct translation', () => {
     const intl = renderHook(
       () => useIntl(),
       { wrapper: createWrapper(container, { mytranslationKey: 'translation message' }) },
     );
 
     expect(intl.result.current.translation('mytranslationKey')).toEqual('translation message');
+  });
+
+  it('renders the correct value', () => {
+    const intl = renderHook(
+      () => useIntl(),
+      { wrapper: createWrapper(container, { mytranslationKey: 'translation {message}' }) },
+    );
+
+    expect(intl.result.current.translation('mytranslationKey', { message: 'here is my value' })).toEqual('translation here is my value');
+  });
+
+  it('renders the Translation tag', () => {
+    render(
+      <Text><Translation id="mytranslationKey" /></Text>,
+      { wrapper: createWrapper(container, { mytranslationKey: 'translation message' }) },
+    );
+
+    expect(screen.getByText('translation message')).toBeTruthy();
   });
 });
