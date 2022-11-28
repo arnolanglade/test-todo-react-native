1c9ec8b3c62ccef2082e151e03f07acf0154da65
Thibaut <thibaut.mejane@gmail.com>
2022-11-28 10:16:27 +0100

    refactor(intl): add describe around tests

--- src/app/i18n/IntlProvider.test.tsx
+++ src/app/i18n/IntlProvider.test.tsx
@@ -8,40 +8,44 @@ import { container } from '../ContainerContext';
 import { createWrapper } from '../testing/WrapperUtils';
 import { Translation, useIntl } from './IntlProvider';
 
-describe('Intl Provider', () => {
-  it('renders the correct translation', () => {
-    const intl = renderHook(
-      () => useIntl(),
-      { wrapper: createWrapper(container, { mytranslationKey: 'translation message' }) },
-    );
+describe('Intl', () => {
+  describe('Translation hook', () => {
+    it('renders the correct translation', () => {
+      const intl = renderHook(
+        () => useIntl(),
+        { wrapper: createWrapper(container, { mytranslationKey: 'translation message' }) },
+      );
 
-    expect(intl.result.current.translation('mytranslationKey')).toEqual('translation message');
+      expect(intl.result.current.translation('mytranslationKey')).toEqual('translation message');
+    });
+
+    it('renders the correct value', () => {
+      const intl = renderHook(
+        () => useIntl(),
+        { wrapper: createWrapper(container, { mytranslationKey: 'translation {message}' }) },
+      );
+
+      expect(intl.result.current.translation('mytranslationKey', { message: 'here is my value' })).toEqual('translation here is my value');
+    });
   });
 
-  it('renders the correct value', () => {
-    const intl = renderHook(
-      () => useIntl(),
-      { wrapper: createWrapper(container, { mytranslationKey: 'translation {message}' }) },
-    );
+  describe('Translation component', () => {
+    it('renders the Translation tag', () => {
+      render(
+        <Text><Translation id="mytranslationKey" /></Text>,
+        { wrapper: createWrapper(container, { mytranslationKey: 'translation message' }) },
+      );
 
-    expect(intl.result.current.translation('mytranslationKey', { message: 'here is my value' })).toEqual('translation here is my value');
-  });
+      expect(screen.getByText('translation message')).toBeTruthy();
+    });
 
-  it('renders the Translation tag', () => {
-    render(
-      <Text><Translation id="mytranslationKey" /></Text>,
-      { wrapper: createWrapper(container, { mytranslationKey: 'translation message' }) },
-    );
+    it('renders the Translation tag with value', () => {
+      render(
+        <Text><Translation id="mytranslationKey" values={{ message: 'here is my value' }} /></Text>,
+        { wrapper: createWrapper(container, { mytranslationKey: 'translation {message}' }) },
+      );
 
-    expect(screen.getByText('translation message')).toBeTruthy();
-  });
-
-  it('renders the Translation tag with value', () => {
-    render(
-      <Text><Translation id="mytranslationKey" values={{ message: 'here is my value' }} /></Text>,
-      { wrapper: createWrapper(container, { mytranslationKey: 'translation {message}' }) },
-    );
-
-    expect(screen.getByText('translation here is my value')).toBeTruthy();
+      expect(screen.getByText('translation here is my value')).toBeTruthy();
+    });
   });
 });
