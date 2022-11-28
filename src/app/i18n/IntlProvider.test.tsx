9d9aa9e343585cd4b21217256a2b8e5ea70bea53
Alexandre <abirba@palo-it.com>
2022-11-28 09:52:38 +0100

    test: add test intl translation message

--- src/app/i18n/IntlProvider.test.tsx
+++ src/app/i18n/IntlProvider.test.tsx
@@ -14,4 +14,13 @@ describe('Intl Provider', () => {
 
     expect(intl.result.current.translation('mytranslationKey')).toEqual('translation message');
   });
+
+  it('renders the todo item details', () => {
+    const intl = renderHook(
+      () => useIntl(),
+      { wrapper: createWrapper(container, { mytranslationKey: 'translation {message}' }) },
+    );
+
+    expect(intl.result.current.translation('mytranslationKey', { message: 'here is my value' })).toEqual('translation here is my value');
+  });
 });
