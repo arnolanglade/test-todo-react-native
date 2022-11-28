88d3f1d83c9d004f474370c59f3d7a2ab7e963bf
arnolanglade <alanglade@palo-it.com>
2022-11-28 09:41:51 +0100

    test: add test to ensure intl provider work as expected

---
+++ src/app/i18n/IntlProvider.test.tsx
@@ -0,0 +1,17 @@
+import {
+  renderHook,
+} from '@testing-library/react-native';
+import { container } from '../ContainerContext';
+import { createWrapper } from '../testing/WrapperUtils';
+import { useIntl } from './IntlProvider';
+
+describe('Intl Provider', () => {
+  it('renders the todo item details', () => {
+    const intl = renderHook(
+      () => useIntl(),
+      { wrapper: createWrapper(container, { mytranslationKey: 'translation message' }) },
+    );
+
+    expect(intl.result.current.translation('mytranslationKey')).toEqual('translation message');
+  });
+});
