dbbe2ff04a931d14dcb0c8bee919150aa0847037
Thibaut <thibaut.mejane@gmail.com>
2022-11-29 16:05:10 +0100

    feat: init TeeModule

---
+++ android/app/src/main/java/com/todo/tee/TeeModule.kt
@@ -0,0 +1,16 @@
+package com.todo.tee
+import com.facebook.react.bridge.NativeModule
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.bridge.ReactContext
+import com.facebook.react.bridge.ReactContextBaseJavaModule
+import com.facebook.react.bridge.ReactMethod
+import android.util.Log
+
+class TeeModule(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
+    override fun getName() = "TeeModule"
+
+    @ReactMethod
+    fun initTee(name: String) {
+        Log.d("TeeModule", "Init Tee: $name")
+    }
+}
