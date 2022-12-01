b702e84f51e5552a96ca6bda015d7bdd99ffee5f
arnolanglade <alanglade@palo-it.com>
2022-12-01 10:45:28 +0100

    feat: add alert

--- src/view/screen/PinScreen.tsx
+++ src/view/screen/PinScreen.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import {
+  Alert,
   Button, Text,
   TextInput,
   View,
@@ -14,7 +15,13 @@ export default function PinScreen() {
   return (
     <View>
       <TextInput onChangeText={(pin) => setSavedPin(pin)} value={savedPin} keyboardType="number-pad" />
-      <Button title="save" onPress={() => SafeVault.savePin(savedPin)} />
+      <Button
+        title="save"
+        onPress={() => {
+          SafeVault.savePin(savedPin);
+          Alert.alert('Le pin est sauvé');
+        }}
+      />
       {isPinVerified && <Text>{isPinVerified ? 'Le pin est bon' : "Le pin n'est pas bon"}</Text>}
       <TextInput onChangeText={(pin) => setVerifyPin(pin)} value={verifyPin} keyboardType="number-pad" />
       <Button
