ccd54a4543171aa15ce79a98acdf7c27b37ce7f7
arnolanglade <alanglade@palo-it.com>
2022-12-05 10:56:10 +0100

    refactor: remove useless code

--- src/app/ServiceContainerContext.tsx
+++ src/app/ServiceContainerContext.tsx
@@ -1,14 +1,13 @@
 import { createContext, useContext } from 'react';
-import { TodoItem, TodoList } from '../domain/TodoList';
-import { queryTodoList, queryTodoItem } from '../service/QueryTodoList';
 
 export type ServiceContainer = {
-  queryTodoList: () => Promise<TodoList>,
-  queryTodoItem: (id:number) => TodoItem
+  // Declare your services here...
+};
+
+export const container = {
+  // Instantiate your services here...
 };
 
 export const ServiceContainerContext = createContext<ServiceContainer>({} as ServiceContainer);
 
 export const useServiceContainer = () => useContext(ServiceContainerContext);
-
-export const container = { queryTodoList, queryTodoItem };
