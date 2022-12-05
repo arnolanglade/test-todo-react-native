3721d979279300e78a72405f9fbbf368dbd8f5c7
arnolanglade <alanglade@palo-it.com>
2022-12-05 09:57:30 +0100

    feat: add online screen

---
+++ src/view/screen/OnlineScreen.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from 'react-native';
+import * as React from 'react';
+
+export default function Online() {
+  return (
+    <View style={[{ height: '100%', width: '100%', justifyContent: 'center' }]}>
+      <Text>Welcome!</Text>
+    </View>
+  );
+}
