490abef4d68ff4698bd73973475418d4e9bc8382
arnolanglade <alanglade@palo-it.com>
2022-12-07 10:41:29 +0100

    feat: chose which app to load, either story book either app

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -8,6 +8,30 @@
  * @format
  */
 
+import React from 'react';
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import Config from 'react-native-config';
+import { container, ServiceContainerContext } from './ServiceContainerContext';
+import NavigatorStack from './navigation/NavigatorStack';
+import IntlProvider from './i18n/IntlProvider';
+import { AuthenticationProvider } from './auth/AuthContext';
 import StorybookUI from '../../storybook';
 
-export default StorybookUI;
+const queryClient = new QueryClient();
+
+function App() {
+  return (
+    <AuthenticationProvider>
+      <ServiceContainerContext.Provider value={container}>
+        <IntlProvider>
+          <QueryClientProvider client={queryClient}>
+            <NavigatorStack />
+          </QueryClientProvider>
+        </IntlProvider>
+      </ServiceContainerContext.Provider>
+    </AuthenticationProvider>
+  );
+}
+
+export default Config.LOAD_STORYBOOK === 'true' ? StorybookUI : App;
