a6f4704e73a82f2256a26c3e4623ed1b829a7820
arnolanglade <alanglade@palo-it.com>
2022-12-07 11:20:22 +0100

    feat: feat autoload stories

--- storybook/index.js
+++ storybook/index.js
@@ -1,6 +1,6 @@
 import { getStorybookUI, configure, addDecorator } from '@storybook/react-native';
 import { withKnobs } from '@storybook/addon-knobs';
-
+import { loadStories } from './storyLoader';
 import './rn-addons';
 
 // enables knobs for all stories
@@ -8,7 +8,7 @@ addDecorator(withKnobs);
 
 // import stories
 configure(() => {
-  require('../src/view/design-system');
+  loadStories();
 }, module);
 
 // Refer to https://github.com/storybookjs/react-native/tree/master/app/react-native#getstorybookui-options
---
+++ storybook/storyLoader.js
@@ -0,0 +1,15 @@
+// Auto-generated file created by react-native-storybook-loader
+// Do not edit.
+//
+// https://github.com/elderfo/react-native-storybook-loader.git
+
+function loadStories() {
+  require('../src/view/design-system/Button/Button.stories');
+}
+
+const stories = ['../src/view/design-system/Button/Button.stories'];
+
+module.exports = {
+  loadStories,
+  stories,
+};
