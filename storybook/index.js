9a900adcfdde6ec11079c0129aba6e7e60400dc1
arnolanglade <alanglade@palo-it.com>
2022-12-07 10:50:33 +0100

    refactor: remove comment

--- storybook/index.js
+++ storybook/index.js
@@ -1,6 +1,3 @@
-// if you use expo remove this line
-// import { AppRegistry } from 'react-native';
-
 import { getStorybookUI, configure, addDecorator } from '@storybook/react-native';
 import { withKnobs } from '@storybook/addon-knobs';
 
@@ -19,9 +16,5 @@ configure(() => {
 const StorybookUIRoot = getStorybookUI({
   asyncStorage: null,
 });
-
-// If you are using React Native vanilla and after installation you don't see your app name here, write it manually.
-// If you use Expo you should remove this line.
-// AppRegistry.registerComponent('%APP_NAME%', () => StorybookUIRoot);
 
 export default StorybookUIRoot;
